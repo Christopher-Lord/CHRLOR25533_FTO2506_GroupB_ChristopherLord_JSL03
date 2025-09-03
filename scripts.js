5e7025e9623a7a85a62aaa9c861a0b0e2b9cf863
Christopher-Lord <lordc42@gmail.com>
2025-09-03 15:38:26 +0200

    Add simple comments

--- scripts.js
+++ scripts.js
@@ -20,6 +20,7 @@ const allTasks = [
   },
 ];
 
+// Function to add a new task to the allTasks array
 function addTask() {
   let newTask = {};
 
@@ -29,28 +30,33 @@ function addTask() {
 
   const validStatuses = ["todo", "doing", "done"];
 
+  // Loop to make sure the entered status is valid
   do {
     newTask.status = prompt("Enter Task Status:").toLowerCase();
 
     if (!validStatuses.includes(newTask.status)) {
       alert("Invalid status. Please enter 'todo', 'doing' or 'done'.");
     }
-    
+
   } while (!validStatuses.includes(newTask.status));
 
+  // Adds new task to end of the main task array based on info entered above
   allTasks.push(newTask);
 }
 
+// Lets user add new tasks until there are either 6 tasks on the list or they cancel out
 while (allTasks.length < 6 && confirm("Would you like to add a new task?")) {
   addTask();
 }
 
+// Lets the user know when their task board is full
 if (allTasks.length >= 6) {
   alert(
     "There are enough tasks on your board, please check them in the console.",
   );
 }
 
+// Filters out all completed tasks (status = done) and assigns them to their own array
 const tasksDone = allTasks.filter((task) => task.status === "done");
 
 console.log("All Tasks:", allTasks);
